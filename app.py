ba65c9ec6d7e1e7281160e21f8b259e97b307637
harsha1464 <136953219+harsha1464@users.noreply.github.com>
2024-06-15 14:24:35 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,100 @@
+from flask import Flask, request, jsonify, send_file, render_template
+import re
+from io import BytesIO
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+import base64
+from transformers import pipeline
+
+# Initialize the sentiment analysis pipeline
+sentiment_pipeline = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
+
+app = Flask(__name__)
+
+@app.route("/test", methods=["GET"])
+def test():
+    return "Test request received successfully. Service is running."
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("landing.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        # Check if the request contains a file (for bulk prediction) or text input
+        if "file" in request.files:
+            # Bulk prediction from CSV file
+            file = request.files["file"]
+            data = pd.read_csv(file)
+
+            predictions, graph = bulk_prediction(data)
+
+            response = send_file(
+                predictions,
+                mimetype="text/csv",
+                as_attachment=True,
+                download_name="Predictions.csv",
+            )
+
+            response.headers["X-Graph-Exists"] = "true"
+
+            response.headers["X-Graph-Data"] = base64.b64encode(
+                graph.getbuffer()
+            ).decode("ascii")
+
+            return response
+
+        elif "text" in request.json:
+            # Single string prediction
+            text_input = request.json["text"]
+            predicted_sentiment = single_prediction(text_input)
+            print(predicted_sentiment)
+            return jsonify({"prediction": predicted_sentiment})
+
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def single_prediction(text_input):
+    analysis = sentiment_pipeline(text_input)[0]
+    sentiment = analysis['label']
+    return "Positive" if sentiment == 'POSITIVE' else "Negative"
+
+def bulk_prediction(data):
+    data['Predicted sentiment'] = data['Sentence'].apply(lambda x: single_prediction(x))
+
+    predictions_csv = BytesIO()
+    data.to_csv(predictions_csv, index=False)
+    predictions_csv.seek(0)
+
+    graph = get_distribution_graph(data)
+
+    return predictions_csv, graph
+
+def get_distribution_graph(data):
+    fig = plt.figure(figsize=(5, 5))
+    colors = ("green", "red")
+    wp = {"linewidth": 1, "edgecolor": "black"}
+    tags = data["Predicted sentiment"].value_counts()
+    explode = (0.01, 0.01)
+
+    tags.plot(
+        kind="pie",
+        autopct="%1.1f%%",
+        shadow=True,
+        colors=colors,
+        startangle=90,
+        wedgeprops=wp,
+        explode=explode,
+        title="Sentiment Distribution",
+        xlabel="",
+        ylabel="",
+    )
+
+    graph = BytesIO()
+    plt.savefig(graph, format="png")
+    plt.close()
+
+    return graph
+
